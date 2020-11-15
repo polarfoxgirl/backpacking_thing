c4e1dcc8d102b96d6d6e5d04c8d1be8dfb6e97f8
Olga Nikonova <catmeggy@gmail.com>
2020-11-16 02:14:01 +0300

    Made park selector work

--- web-app/src/components/ParkSelector.tsx
+++ web-app/src/components/ParkSelector.tsx
@@ -1,27 +1,60 @@
 import React, { useState, useEffect } from 'react'
 import {
-    BrowserRouter as Router,
-    Link
+    Redirect,
   } from "react-router-dom";
+
+type ParkSelectState = {
+  parkName: string,
+  redirect: boolean,
+}
+
+class ParkSelectForm extends React.Component<{}, ParkSelectState> {
+  constructor(props: {}) {
+    super(props);
+
+    this.state = {parkName: 'rocky_mountain', redirect: false};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event:  React.ChangeEvent<HTMLSelectElement>) {
+    this.setState({parkName: event.target.value});
+  }
+
+  handleSubmit(event:  React.FormEvent<HTMLFormElement>) {
+    this.setState({redirect: true});
+    event.preventDefault();
+  }
+
+  render() {
+    if (this.state.redirect) {
+      return (<Redirect to={"/park/" + this.state.parkName}/>);
+    }
+
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          <p>Select your destination:</p>
+          <select value={this.state.parkName} onChange={this.handleChange}>
+            <option value="rocky_mountain">Rocky Mountain National Park</option>
+            <option value="olympic">Olympic National Park</option>
+            <option value="zion">Zion National Park</option>
+          </select>
+        </label>
+        <input type="submit" value="Submit" className="button special big" />
+      </form>
+    );
+  }
+}
+
 
 class ParkSelector extends React.Component {
     render() {
         return (
           <section id="banner">
             <h2>Where to?</h2>
-            <form method="post" action="#">
-              <div className="select-wrapper">
-                <select name="category" id="category">
-                  <option value="">- Select your destination area -</option>
-                  <option value="rocky_mountain">Rocky Mountain National Park</option>
-                  <option value="olympic">Olympic National Park</option>
-                  <option value="zion">Zion National Park</option>
-                </select>
-              </div>
-              <ul className="actions">
-                <Link to="#" className="button special big">Get Trails</Link>
-              </ul>
-            </form>
+            <ParkSelectForm/>
           </section>
         );
     }
