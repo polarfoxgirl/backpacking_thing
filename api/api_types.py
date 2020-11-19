2e6711241887a62c5e0d63b809ec1432c376ad8d
Olga Nikonova <catmeggy@gmail.com>
2020-11-19 04:11:28 +0300

    Added API call for trail details

--- api/api_types.py
+++ api/api_types.py
@@ -1,15 +1,18 @@
 from enum import Enum
 from typing import NamedTuple, List
 
+
 class DifficultyLevel(Enum):
   easy = 1
   medium = 2
   hard = 3
 
+
 class ParkTrailsRequest(NamedTuple):
   days: int
   difficulty: DifficultyLevel
   party_size: int
+
 
 class TrailItemInfo(NamedTuple):
   tid: int
@@ -18,6 +21,12 @@ class TrailItemInfo(NamedTuple):
   max_mileage: int
   camps: List[str]
 
+
 class ParkTrailsResponse(NamedTuple):
   trails: List[TrailItemInfo]
+
+
+class TrailDetailsResponse(NamedTuple):
+  name: str
+  info: str
 
--- api/main_service.py
+++ api/main_service.py
@@ -2,7 +2,12 @@ from flask import Flask, request, abort
 from werkzeug.datastructures import ImmutableMultiDict
 import json
 
-from api_types import DifficultyLevel, ParkTrailsResponse, ParkTrailsRequest
+from api_types import (
+  DifficultyLevel,
+  ParkTrailsResponse,
+  ParkTrailsRequest,
+  TrailDetailsResponse,
+)
 from trail_selector import select_trails
 
 app = Flask(__name__)
@@ -41,8 +46,16 @@ def parse_park_trails_args(args : ImmutableMultiDict) -> ParkTrailsRequest:
 
   return ParkTrailsRequest(days, difficulty, party_size)
 
+
 @app.route('/api/0/park_trails/<park_name>')
-def park_trails(park_name: str):
+def park_trails(park_name: str) -> str:
   trails_request = parse_park_trails_args(request.args)
   response = select_trails(trails_request)
+  return json.dumps(response._asdict())
+
+
+@app.route('/api/0/trail/<int:tid>')
+def trail_details(tid: int) -> str:
+  trail_name = f"Trail #{tid}"
+  response = TrailDetailsResponse(trail_name, "Some useful information about the trail here")
   return json.dumps(response._asdict())
